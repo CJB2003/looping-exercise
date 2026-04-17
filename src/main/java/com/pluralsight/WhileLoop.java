8b5f6ebcf97ab2c570bdad2994ea85ba3af055fa
chrisbunny <christopherjbunny@gmail.com>
2026-04-17 11:56:02 -0500

    Initialize variable and added a while loop

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        //Initializing variable i
+        int i = 0;
+
+        //while loop that continues printing "I love Java" til it iterates 5 times.
+        while(i < 5) {
+            System.out.println("I love Java");
+        }
+    }
+}
